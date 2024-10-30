698a25ac9a8d30e88a07f9f7c107c438a4d0940b
Abhishek <147073711+CodewithAbhi14@users.noreply.github.com>
2024-10-31 03:13:28 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://workout-tracker-backend-eo0w.onrender.com',
         changeOrigin: true,
       },
     },
